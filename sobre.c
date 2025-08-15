f96af1233f41bfe3ba8e2bc8df4cc7be41a4f9cb
HelenaMariano2025 <helena.mariano.123@ufrn.edu.br>
2025-08-15 16:53:27 -0300

    feat: add sobre

---
+++ sobre.c
@@ -0,0 +1,19 @@
+void sobre(void) {
+
+    printf("_______________________________________________________________________________\n");
+    printf("|                                                                             |\n");
+    printf("|                               Informações                                   |\n");
+    printf("|_____________________________________________________________________________|\n");
+    printf("|                                                                             |\n");
+    printf("|  Sistema desenvolvido para gestão completa de uma barbearia, contendo:      |\n");
+    printf("|  - Cadastro e gestão de clientes                                            |\n");
+    printf("|  - Catálogo de serviços oferecidos                                          |\n");
+    printf("|  - Agendamento de horários                                                  |\n");
+    printf("|  - Geração de relatórios financeiros                                        |\n");
+    printf("|  - Gestão de colaboradores                                                  |\n");
+    printf("|                                                                             |\n");
+    printf("|  Projeto desenvolvido como requisito para a disciplina DCT1106 -            |\n");
+    printf("|  Programação, aplicando os conceitos aprendidos em sala de aula.            |\n");
+    printf("|_____________________________________________________________________________|\n\n");
+
+} 
