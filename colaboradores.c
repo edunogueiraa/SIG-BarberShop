1ffc0c9f68886a27c3b4c6c16d7f54396f1220d6
HelenaMariano2025 <helena.mariano.123@ufrn.edu.br>
2025-08-15 16:49:25 -0300

    feat: add colaboradores

---
+++ colaboradores.c
@@ -0,0 +1,19 @@
+void colaboradores(void) {
+
+    printf("______________________________________________________________\n");
+    printf("|                                                            |\n");
+    printf("|            Este projeto foi desenvolvido por:              |\n");
+    printf("|____________________________________________________________|\n");
+    printf("|                                                            |\n");
+    printf("|    Eduardo Nogueira                                        |\n");
+    printf("|    E-mail: eduardonogueira105@gmail.com                    |\n");
+    printf("|    Redes sociais: edu_nogueira_silva                       |\n");
+    printf("|    Git: https://github.com/edunogueiraa/SIG-BarberShop.git |\n");
+    printf("|                                                            |\n");
+    printf("|    Helena Dantas                                           |\n");
+    printf("|    E-mail: dantashelena502@gmail.com                       |\n");
+    printf("|    Redes sociais: h.mariano_                               |\n");
+    printf("|    Git: https://github.com/helenaMariano2025               |\n");
+    printf("|____________________________________________________________|\n\n");
+
+} 
