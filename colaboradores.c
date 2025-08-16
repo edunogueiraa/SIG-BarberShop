874b04c6be75840dde9e29471ca14161b308817f
edunogueiraa <eduardonogueira105@gmail.com>
2025-08-16 19:35:44 -0300

    refactor: modificações colaboradores

--- colaboradores.c
+++ colaboradores.c
@@ -1,5 +1,8 @@
-void colaboradores(void) {
+#include <stdio.h>
+#include <stdlib.h>
 
+void colaboradores(void) {
+    system("clear||cls");
     printf("______________________________________________________________\n");
     printf("|                                                            |\n");
     printf("|            Este projeto foi desenvolvido por:              |\n");
@@ -8,12 +11,15 @@ void colaboradores(void) {
     printf("|    Eduardo Nogueira                                        |\n");
     printf("|    E-mail: eduardonogueira105@gmail.com                    |\n");
     printf("|    Redes sociais: edu_nogueira_silva                       |\n");
-    printf("|    Git: https://github.com/edunogueiraa/SIG-BarberShop.git |\n");
+    printf("|    Git: https://github.com/edunogueiraa                    |\n");
     printf("|                                                            |\n");
     printf("|    Helena Dantas                                           |\n");
     printf("|    E-mail: dantashelena502@gmail.com                       |\n");
     printf("|    Redes sociais: h.mariano_                               |\n");
     printf("|    Git: https://github.com/helenaMariano2025               |\n");
     printf("|____________________________________________________________|\n\n");
+
+    printf("\n>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 
 } 
