042f6d927001cb8ccabd4d7509709bfef6f3fab0
edunogueiraa <eduardonogueira105@gmail.com>
2025-08-17 18:29:28 -0300

    refactor: correcao de telas

--- agendamento.c
+++ agendamento.c
@@ -5,17 +5,27 @@
 int agendamento() {
     system("clear||cls");
     int opcaoAgendamento;
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|               Agendamentos               |\n");
-    printf("|__________________________________________|\n");
-    printf("|                                          |\n");
-    printf("|           1 Cadastrar                    |\n");
-    printf("|           2 Listar                       |\n");
-    printf("|           3 Atualizar                    |\n");
-    printf("|           4 Deletar                      |\n");
-    printf("|           0 Sair                         |\n");
-    printf("|__________________________________________|\n\n");
+    printf("_________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|      _____ _____ _____       ______            _               _____ _                          |\n");
+    printf("|     /  ___|_   _|  __ \\      | ___ \\          | |             /  ___| |                         |\n");
+    printf("|     \\ `--.  | | | |  \\/______| |_/ / __ _ _ __| |__   ___ _ __\\ `--.| |__   ___  _ __           |\n");
+    printf("|      `--. \\ | | | | __|______| ___ \\/ _` | '__| '_ \\ / _ \\ '__|`--. \\ '_ \\ / _ \\| '_ \\          |\n");
+    printf("|     /\\__/ /_| |_| |_\\ \\      | |_/ / (_| | |  | |_) |  __/ |  /\\__/ / | | | (_) | |_) |         |\n");
+    printf("|     \\____/ \\___/ \\____/      \\____/ \\__,_|_|  |_.__/ \\___|_|  \\____/|_| |_|\\___/| .__/          |\n");
+    printf("|                                                                                 | |             |\n");
+    printf("|                                                                                 |_|             |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         MENU AGENDAMENTO                                        |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                   1 Cadastrar                                                   |\n");
+    printf("|                                   2 Listar                                                      |\n");
+    printf("|                                   3 Atualizar                                                   |\n");
+    printf("|                                   4 Deletar                                                     |\n");
+    printf("|                                   0 Sair                                                        |\n");
+    printf("|_________________________________________________________________________________________________|\n\n");
 
     printf("Digite a opção desejada: ");
     scanf("%d", &opcaoAgendamento);
@@ -28,10 +38,13 @@ int agendamento() {
 void cadastroAgendamento(void) {
     system("clear||cls");
     printf("\n");
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|         Cadastro de Agendamento          |\n");
-    printf("|__________________________________________|\n");
+    printf("___________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         CADASTRO AGENDAMENTO                                    |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+
+    printf("\nDigite o CPF do cliente (apenas numeros): \n");
+    printf("Digite o ID do serviço:  \n");
 
     printf("\n>>> Tecle <ENTER> para continuar...\n");
     getchar();
@@ -40,10 +53,12 @@ void cadastroAgendamento(void) {
 void listarAgendamento(void) {
     system("clear||cls");
     printf("\n");
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|            Listar Agendamento            |\n");
-    printf("|__________________________________________|\n");
+    printf("___________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         LISTAR AGENDAMENTO                                      |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+
+    printf("\nDigite o ID do agendamento: \n");
 
     printf("\n>>> Tecle <ENTER> para continuar...\n");
     getchar();
@@ -52,10 +67,12 @@ void listarAgendamento(void) {
 void atualizarAgendamento(void) {
     system("clear||cls");
     printf("\n");
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|          Atualizar Agendamento           |\n");
-    printf("|__________________________________________|\n");
+    printf("___________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         ATUALIZAR AGENDAMENTO                                   |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+
+    printf("\nDigite o ID do agendamento: \n");
 
     printf("\n>>> Tecle <ENTER> para continuar...\n");
     getchar();
@@ -64,10 +81,12 @@ void atualizarAgendamento(void) {
 void deletarAgendamento(void) {
     system("clear||cls");
     printf("\n");
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|            Deletar Agendamento           |\n");
-    printf("|__________________________________________|\n");
+    printf("___________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         DELETAR AGENDAMENTO                                     |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+
+    printf("\nDigite o ID do agendamento: \n");
 
     printf("\n>>> Tecle <ENTER> para continuar...\n");
     getchar();
