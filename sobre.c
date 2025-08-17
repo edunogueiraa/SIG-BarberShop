e80074634cd40a867016814696a449c1035dffa1
edunogueiraa <eduardonogueira105@gmail.com>
2025-08-17 18:31:56 -0300

    refactor: correcao de telas

--- sobre.c
+++ sobre.c
@@ -3,23 +3,34 @@
 
 void sobre(void) {
     system("clear||cls");
-    printf("_______________________________________________________________________________\n");
-    printf("|                                                                             |\n");
-    printf("|                               Informações                                   |\n");
-    printf("|_____________________________________________________________________________|\n");
-    printf("|                                                                             |\n");
-    printf("|  Sistema desenvolvido para gestão completa de uma barbearia, contendo:      |\n");
-    printf("|  - Cadastro e gestão de clientes                                            |\n");
-    printf("|  - Catálogo de serviços oferecidos                                          |\n");
-    printf("|  - Agendamento de horários                                                  |\n");
-    printf("|  - Geração de relatórios financeiros                                        |\n");
-    printf("|  - Gestão de colaboradores                                                  |\n");
-    printf("|                                                                             |\n");
-    printf("|  Projeto desenvolvido como requisito para a disciplina DCT1106 -            |\n");
-    printf("|  Programação, aplicando os conceitos aprendidos em sala de aula.            |\n");
-    printf("|_____________________________________________________________________________|\n\n");
+    printf("_________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|      _____ _____ _____       ______            _               _____ _                          |\n");
+    printf("|     /  ___|_   _|  __ \\      | ___ \\          | |             /  ___| |                         |\n");
+    printf("|     \\ `--.  | | | |  \\/______| |_/ / __ _ _ __| |__   ___ _ __\\ `--.| |__   ___  _ __           |\n");
+    printf("|      `--. \\ | | | | __|______| ___ \\/ _` | '__| '_ \\ / _ \\ '__|`--. \\ '_ \\ / _ \\| '_ \\          |\n");
+    printf("|     /\\__/ /_| |_| |_\\ \\      | |_/ / (_| | |  | |_) |  __/ |  /\\__/ / | | | (_) | |_) |         |\n");
+    printf("|     \\____/ \\___/ \\____/      \\____/ \\__,_|_|  |_.__/ \\___|_|  \\____/|_| |_|\\___/| .__/          |\n");
+    printf("|                                                                                 | |             |\n");
+    printf("|                                                                                 |_|             |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                           Informações                                           |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+    printf("|                                                                                                 |\n");
+    printf("|                      Sistema desenvolvido para gestão completa de uma barbearia, contendo:      |\n");
+    printf("|                      - Cadastro e gestão de clientes                                            |\n");
+    printf("|                      - Catálogo de serviços oferecidos                                          |\n");
+    printf("|                      - Agendamento de horários                                                  |\n");
+    printf("|                      - Geração de relatórios financeiros                                        |\n");
+    printf("|                      - Gestão de colaboradores                                                  |\n");
+    printf("|                                                                                                 |\n");
+    printf("|                      Projeto desenvolvido como requisito para a disciplina DCT1106 -            |\n");
+    printf("|                      Programação, aplicando os conceitos aprendidos em sala de aula.            |\n");
+    printf("|_________________________________________________________________________________________________|\n\n");
 
     printf("\n>>> Tecle <ENTER> para continuar...\n");
+    getchar();
     getchar();
 
 } 
