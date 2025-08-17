21e10072f76dc6e6b80d609d6d24b41afda284af
edunogueiraa <eduardonogueira105@gmail.com>
2025-08-17 18:29:51 -0300

    refactor: correcao de telas

--- cliente.c
+++ cliente.c
@@ -5,17 +5,27 @@
 int cliente() {
     system("clear||cls");
     int opcaoCliente;
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|                Clientes                  |\n");
-    printf("|__________________________________________|\n");
-    printf("|                                          |\n");
-    printf("|           1 Cadastrar                    |\n");
-    printf("|           2 Listar                       |\n");
-    printf("|           3 Atualizar                    |\n");
-    printf("|           4 Deletar                      |\n");
-    printf("|           0 Sair                         |\n");
-    printf("|__________________________________________|\n\n");
+    printf("_________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|      _____ _____ _____       ______            _               _____ _                          |\n");
+    printf("|     /  ___|_   _|  __ \\      | ___ \\          | |             /  ___| |                         |\n");
+    printf("|     \\ `--.  | | | |  \\/______| |_/ / __ _ _ __| |__   ___ _ __\\ `--.| |__   ___  _ __           |\n");
+    printf("|      `--. \\ | | | | __|______| ___ \\/ _` | '__| '_ \\ / _ \\ '__|`--. \\ '_ \\ / _ \\| '_ \\          |\n");
+    printf("|     /\\__/ /_| |_| |_\\ \\      | |_/ / (_| | |  | |_) |  __/ |  /\\__/ / | | | (_) | |_) |         |\n");
+    printf("|     \\____/ \\___/ \\____/      \\____/ \\__,_|_|  |_.__/ \\___|_|  \\____/|_| |_|\\___/| .__/          |\n");
+    printf("|                                                                                 | |             |\n");
+    printf("|                                                                                 |_|             |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         MENU CLIENTE                                            |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                   1 Cadastrar                                                   |\n");
+    printf("|                                   2 Listar                                                      |\n");
+    printf("|                                   3 Atualizar                                                   |\n");
+    printf("|                                   4 Deletar                                                     |\n");
+    printf("|                                   0 Sair                                                        |\n");
+    printf("|_________________________________________________________________________________________________|\n\n");
 
     printf("Digite a opção desejada: ");
     scanf("%d", &opcaoCliente);
@@ -28,10 +38,16 @@ int cliente() {
 void cadastroCliente(void) {
     system("clear||cls");
     printf("\n");
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|           Cadastro de Cliente            |\n");
-    printf("|__________________________________________|\n");
+    printf("___________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         CADASTRO CLIENTE                                        |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+        
+    printf("\nNome completo: \n");
+    printf("CPF: \n");
+    printf("E-mail: \n");
+    printf("Data de Nascimento (dd/mm/aaaa): \n");
+    printf("Celular  (apenas números): \n");
 
     printf("\n>>> Tecle <ENTER> para continuar...\n");
     getchar();
@@ -40,11 +56,12 @@ void cadastroCliente(void) {
 void listarCliente(void) {
     system("clear||cls");
     printf("\n");
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|             Listar Cliente               |\n");
-    printf("|__________________________________________|\n");
+    printf("___________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         LISTAR CLIENTE                                          |\n");
+    printf("|_________________________________________________________________________________________________|\n");
 
+    printf("\nInforme o CPF (apenas numeros): \n");
     printf("\n>>> Tecle <ENTER> para continuar...\n");
     getchar();
 }
@@ -52,11 +69,12 @@ void listarCliente(void) {
 void atualizarCliente(void) {
     system("clear||cls");
     printf("\n");
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|            Atualizar Cliente             |\n");
-    printf("|__________________________________________|\n");
+    printf("___________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         ATUALIZAR CLIENTE                                       |\n");
+    printf("|_________________________________________________________________________________________________|\n");
 
+    printf("\nInforme o CPF (apenas numeros): \n");
     printf("\n>>> Tecle <ENTER> para continuar...\n");
     getchar();
 }
@@ -64,11 +82,12 @@ void atualizarCliente(void) {
 void deletarCliente(void) {
     system("clear||cls");
     printf("\n");
-    printf("____________________________________________\n");
-    printf("|                                          |\n");
-    printf("|             Deletar Cliente              |\n");
-    printf("|__________________________________________|\n");
+    printf("___________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                         DELETAR CLIENTE                                         |\n");
+    printf("|_________________________________________________________________________________________________|\n");
 
+    printf("\nInforme o CPF (apenas numeros): \n");
     printf("\n>>> Tecle <ENTER> para continuar...\n");
     getchar();
 }
