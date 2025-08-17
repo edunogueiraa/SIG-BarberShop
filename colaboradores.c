06ba1eb6a1610fcc4345e6fb22c79e1aff76ba8e
edunogueiraa <eduardonogueira105@gmail.com>
2025-08-17 18:30:15 -0300

    refactor: correcao de telas

--- colaboradores.c
+++ colaboradores.c
@@ -3,23 +3,34 @@
 
 void colaboradores(void) {
     system("clear||cls");
-    printf("______________________________________________________________\n");
-    printf("|                                                            |\n");
-    printf("|            Este projeto foi desenvolvido por:              |\n");
-    printf("|____________________________________________________________|\n");
-    printf("|                                                            |\n");
-    printf("|    Eduardo Nogueira                                        |\n");
-    printf("|    E-mail: eduardonogueira105@gmail.com                    |\n");
-    printf("|    Redes sociais: edu_nogueira_silva                       |\n");
-    printf("|    Git: https://github.com/edunogueiraa                    |\n");
-    printf("|                                                            |\n");
-    printf("|    Helena Dantas                                           |\n");
-    printf("|    E-mail: dantashelena502@gmail.com                       |\n");
-    printf("|    Redes sociais: h.mariano_                               |\n");
-    printf("|    Git: https://github.com/helenaMariano2025               |\n");
-    printf("|____________________________________________________________|\n\n");
+    printf("_________________________________________________________________________________________________\n");
+    printf("|                                                                                                 |\n");
+    printf("|      _____ _____ _____       ______            _               _____ _                          |\n");
+    printf("|     /  ___|_   _|  __ \\      | ___ \\          | |             /  ___| |                         |\n");
+    printf("|     \\ `--.  | | | |  \\/______| |_/ / __ _ _ __| |__   ___ _ __\\ `--.| |__   ___  _ __           |\n");
+    printf("|      `--. \\ | | | | __|______| ___ \\/ _` | '__| '_ \\ / _ \\ '__|`--. \\ '_ \\ / _ \\| '_ \\          |\n");
+    printf("|     /\\__/ /_| |_| |_\\ \\      | |_/ / (_| | |  | |_) |  __/ |  /\\__/ / | | | (_) | |_) |         |\n");
+    printf("|     \\____/ \\___/ \\____/      \\____/ \\__,_|_|  |_.__/ \\___|_|  \\____/|_| |_|\\___/| .__/          |\n");
+    printf("|                                                                                 | |             |\n");
+    printf("|                                                                                 |_|             |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                 Este projeto foi desenvolvido por:                              |\n");
+    printf("|_________________________________________________________________________________________________|\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                 Eduardo Nogueira                                                |\n");
+    printf("|                                 E-mail: eduardonogueira105@gmail.com                            |\n");
+    printf("|                                 Redes sociais: edu_nogueira_silva                               |\n");
+    printf("|                                 Git: https://github.com/edunogueiraa                            |\n");
+    printf("|                                                                                                 |\n");
+    printf("|                                 Helena Dantas                                                   |\n");
+    printf("|                                 E-mail: dantashelena502@gmail.com                               |\n");
+    printf("|                                 Redes sociais: h.mariano_                                       |\n");
+    printf("|                                 Git: https://github.com/helenaMariano2025                       |\n");
+    printf("|_________________________________________________________________________________________________|\n\n");
 
     printf("\n>>> Tecle <ENTER> para continuar...\n");
+    getchar();
     getchar();
 
 } 
