11887429565fde9fcb13db2d0e50a07c9989beb7
Eduardo Nogueira <102708905+edunogueiraa@users.noreply.github.com>
2025-10-04 22:00:15 +0000

    feat: Adicionando tipos estruturados estoque

--- modulos/include/estoqueProdutos.h
+++ modulos/include/estoqueProdutos.h
@@ -1,6 +1,16 @@
+struct estoque {
+    FILE * arquivoEstoque;
+    char nome[50];
+    char id[50];
+    char tipo[50];
+    char valor[50];
+};
+
+typedef struct estoque Estoque;
+
 void telaEstoque(void);
-void cadastroEstoque(void);
-void exibeProduto(void);
+void cadastroEstoque(Estoque);
+void exibeProduto(Estoque);
 void atualizarEstoque(void);
 void deletarEstoque(void);
 void opcaoEstoque(void);
--- modulos/include/servicos.h
+++ modulos/include/servicos.h
@@ -9,8 +9,8 @@ struct servico {
 typedef struct servico Servico;
 
 void telaServicos(void);
-void cadastroServicos(void);
-void exibeServicos(void);
+void cadastroServicos(Servico);
+void exibeServicos(Servico);
 void atualizarServicos(void);
 void deletarServicos(void);
 void opcaoServicos(void);
