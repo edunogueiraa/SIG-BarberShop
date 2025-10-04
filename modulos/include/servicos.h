16ce9d0ed17625ad26c4e8bb7bce8bfc94d072e1
Eduardo Nogueira <102708905+edunogueiraa@users.noreply.github.com>
2025-10-04 21:37:46 +0000

    feat: adicionando tipos estruturados servicos

--- modulos/include/servicos.h
+++ modulos/include/servicos.h
@@ -1,3 +1,13 @@
+struct servico {
+    FILE *arquivoServico;
+    char nome[50];
+    char id[50];
+    char valor[50];
+    char duracao[50];
+};
+
+typedef struct servico Servico;
+
 void telaServicos(void);
 void cadastroServicos(void);
 void exibeServicos(void);
