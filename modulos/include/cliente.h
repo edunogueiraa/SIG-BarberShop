55d3f139772accfffc4858e2f99900df2940535a
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-10-05 10:10:34 -0300

    feat: add struct cliente

--- modulos/include/cliente.h
+++ modulos/include/cliente.h
@@ -1,3 +1,14 @@
+struct cliente {
+    FILE * arquivoCliente;
+    char nome[50];
+    char cpf[50];
+    char email[50];
+    char data[50];
+    char celular[50];
+};
+
+typedef struct cliente Cliente;
+
 void telaCliente(void);
 void cadastroCliente(void);
 void exibeCliente(void);
