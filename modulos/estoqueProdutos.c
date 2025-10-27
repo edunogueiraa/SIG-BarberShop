1c37f589d18761a150834256c6bedfc2a55c527f
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-10-27 09:43:52 -0300

    feat: importa biblioteca e remove depreciados

--- modulos/estoqueProdutos.c
+++ modulos/estoqueProdutos.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include "include/estoqueProdutos.h"
+#include "include/utils.h"
 
 // Assinatura de funções
 void cadastrarProduto(Estoque * estoque);
@@ -11,8 +12,6 @@ void exibirProduto(char idProduto[]);
 void atualizarProduto(char idProduto[], int opcao);
 void deletarProduto(char idProduto[]);
 void excluirBancoEstoque(void);
-void trocarArquivosEstoque(char antigo[], char novo[]);
-void criarDiretorio(void);
 
 void telaEstoque(void) {
     system("clear||cls");
@@ -358,41 +357,9 @@ void excluirBancoEstoque(void) {
     fclose(arquivo);
     fclose(arquivoTemp);
 
-    trocarArquivosEstoque("./dados/estoque.bin", "./dados/estoque_temp.bin");
+    trocaArquivos("./dados/estoque.bin", "./dados/estoque_temp.bin");
 
     printf("Limpeza do banco concluída com sucesso!\n");
     printf("Estoques mantidos: %d\n", estoquesMantidos);
     printf("Estoques removidos: %d\n", estoquesRemovidos);
-}
-
-void trocarArquivosEstoque(char antigo[], char novo[]) {
-    int retorno = remove(antigo);
-    if (retorno != 0) {
-        printf("Houve um erro na exclusão. O programa será finalizado.");
-        printf("\n>>> Tecle <ENTER> para encerrar o programa.\n");
-        getchar();
-        exit(1);
-    }
-
-    int renomeacao = rename(novo, antigo);
-    if (renomeacao != 0) {
-        printf("Houve um erro na renomeação do arquivo. O programa será finalizado.");
-        printf("\n>>> Tecle <ENTER> para encerrar o programa.\n");
-        getchar();
-        exit(1);
-    }
-    return;
-}
-
-void criaDiretorio(void) {
-    // Função adaptada de:
-    // https://linux.die.net/man/2/mkdir e https://stackoverflow.com/questions/7430248/creating-a-new-directory-in-c
-    // Criando diretório para armazenamento de dados
-    int status = mkdir("dados", 0700);
-    if (status < 0 && errno != EEXIST)
-    {
-        printf("Houve um erro na criação do diretório de armazenamento de dados. O programa será finalizado.");
-        printf("\n>>> Tecle <ENTER> para encerrar o programa.\n");
-        getchar();
-    }
 }
