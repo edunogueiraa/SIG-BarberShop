c2819b6051f75698396737df5f3e1ba06066e467
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-10-28 15:10:13 -0300

    remove: funcoes nao exportadas

--- modulos/include/utils.h
+++ modulos/include/utils.h
@@ -1,8 +1,6 @@
 #define True 1
 #define False 0
 
-int validaEmail(char *email);
-int validaNome(char *nome);
 int validaCelular(char *celular);
 void recebeNome(char ponteiroNome[], char tipo[]);
 void recebeCpf(char ponteiroCpf[]);
