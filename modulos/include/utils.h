666d9457c1be09659312676fdbb3cef0b7eaac44
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-11-03 09:24:43 -0300

    feat: funcoes de exibicao

--- modulos/include/utils.h
+++ modulos/include/utils.h
@@ -12,7 +12,8 @@ void recebeValor(char ponteiroValor[]);
 void recebeDuracaoTempo(char ponteiroDuracao[]); 
 void recebeHora(char ponteiroHora[]);
 void recebeOpcao(char ponteiroOpcao[]);
-
+void exibeValor(char *valor);
+void formataCpf(char *cpf);
 void verificaArquivo(FILE * arquivo);
 void verificaArquivoTemporario(FILE * arquivoTemp);
 void trocaArquivos(char antigo[], char novo[]);
