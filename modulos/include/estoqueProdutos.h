712d0899d709d63898cc8649330322cfe5e67f95
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-12-02 13:42:15 -0300

    minor: add assinaturas

--- modulos/include/estoqueProdutos.h
+++ modulos/include/estoqueProdutos.h
@@ -15,12 +15,15 @@ struct estoque {
 void telaEstoque(void);
 void cadastroEstoque(void);
 void exibeProduto(void);
-void atualizarEstoque(void);
-void deletarEstoque(void);
+void listaEstoque(void);
+void atualizaProduto(void);
+void deletaProduto(void);
+void limparBancoEstoque(void);
 void opcaoEstoque(void);
 
 void cadastrarProduto(Estoque *estoque);
 int exibirProduto(char idProduto[]);
+void exibirDadosProduto(Estoque* estoque);
 void listarProdutos(Estoque *lista);
 void listarProdutosPreco(Estoque *lista, float minimo, float maximo);
 void atualizarProduto(char idProduto[], int opcao);
@@ -28,5 +31,6 @@ void deletarProduto(char idProduto[]);
 void excluirBancoEstoque(void);
 Estoque *gerarListaEstoque(void);
 Estoque *gerarListaOrdemPreco(void);
+
 void liberarListaEstoque(Estoque **lista);
 int verificaEstoque(char *idProduto);
